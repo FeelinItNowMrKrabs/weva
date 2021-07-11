57fd72b1b51a9506816db26a327d720ddc8d14ba
Petr Yakovlev <yakovlev123peter@gmail.com>
2021-07-11 19:20:39 +0500

    Added blue square

---
+++ lib/src/ screens/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      width: double.infinity,
+      height: 500,
+      color: Colors.amber,
+      child: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          color: Colors.purple,
+        ),
+      ),
+    );
+  }
+}
